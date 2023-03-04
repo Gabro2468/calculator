d83a1f76e1d2e842a0db398875664adb8faf569a
Gabro2468 <mylitle43@gmail.com>
2023-03-04 20:21:15 +0100

    Add functionality to left-buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Functions for math 
-let add = (a,b) => a+b
-let substract = (a,b) => a-b
-let multiply = (a,b) => a*b
-let divide = (a,b) => a/b
+const add = (a,b) => a+b
+const substract = (a,b) => a-b
+const multiply = (a,b) => a*b
+const divide = (a,b) => a/b
 
 let operate = (a,b,operator) =>{
     switch(true){
@@ -13,33 +13,86 @@ let operate = (a,b,operator) =>{
         case operator === "*":
             return multiply(a,b)
         case operator === "/":
-            return divide(a,b)
+            if (b == 0){
+                return "Error"
+            }
+            else{
+                return divide(a,b)
+            }
         default:
             console.log("Enter a proper one")
     }
 }
 
+// Adding event listeners for numbers
 const numbersId = ["#zero","#one","#two","#three","#four","#five","#six","#seven","#eight","#nine"]
 let numbers = []
 const screen = document.querySelector("#screen")
+let numberA = ""
+let numberB = ""
 
-for(let i=0; i<=10; i++){
+for(let i = 0; i < 10; i++){
     numbers[i] = document.querySelector(numbersId[i]);
     numbers[i].addEventListener('click', () => {
-        a = i
-        screen.innerHTML = a
+        if (operator === ""){
+            numberA += i
+            screen.innerHTML = numberA
+        }
+        else{
+            numberB += i
+            screen.innerHTML = numberB
+        }
     })
 }
 
+// Adding event listeners for math methods 
 const divideOperator = document.querySelector("#divide")
 const multiplyOperator = document.querySelector("#multiply")
 const substractOperator = document.querySelector("#substract")
 const addOperator = document.querySelector("#add")
 const equalOperator = document.querySelector("#equal")
 let operator = ""
+let score = 0
  
 divideOperator.addEventListener("click", () => {
-    operator = "/"
-    divideOperator.target.style.backgroundColor = 'orange'
-    divideOperator.style.color = 'azure'
+    operator = "/";
+    divideOperator.className = "button-right-active";
+    multiplyOperator.className = "button-right" 
+    substractOperator.className = "button-right"
+    addOperator.className = "button-right"
+})
+
+multiplyOperator.addEventListener("click", () => {
+    operator = "*";
+    multiplyOperator.className = "button-right-active";
+    divideOperator.className = "button-right" 
+    substractOperator.className = "button-right"
+    addOperator.className = "button-right"
+})
+
+substractOperator.addEventListener("click", () => {
+    operator = "-";
+    substractOperator.className = "button-right-active";
+    divideOperator.className = "button-right" 
+    multiplyOperator.className = "button-right"
+    addOperator.className = "button-right"
+})
+
+addOperator.addEventListener("click", () => {
+    operator = "+";
+    addOperator.className = "button-right-active";
+    divideOperator.className = "button-right" 
+    substractOperator.className = "button-right"
+    multiplyOperator.className = "button-right"
+})
+
+equalOperator.addEventListener("click", () => {
+    screen.innerHTML = operate(parseInt(numberA),parseInt(numberB),operator)
+    score = operate(parseInt(numberA),parseInt(numberB),operator)
+    numberA = score
+    numberB = ""
+    addOperator.className = "button-right";
+    substractOperator.className = "button-right"
+    multiplyOperator.className = "button-right"
+    divideOperator.className = "button-right" 
 })
